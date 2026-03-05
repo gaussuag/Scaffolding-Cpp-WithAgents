60eb3ac769931d73fa9607a999c2545dda6fc1db
gaussuag <502415552@qq.com>
2026-03-05 10:51:02 +0800

    fix: CMake fix

--- cmake/CompilerOptions.cmake
+++ cmake/CompilerOptions.cmake
@@ -1,10 +1,10 @@
 if(MSVC)
     add_compile_definitions(_CRT_SECURE_NO_WARNINGS _UNICODE UNICODE)
     
-    set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreadedDebug$<$<CONFIG:Debug>:D>$<$<CONFIG:Release>:>")
+    set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreadedDebugDLL")
     
-    string(APPEND CMAKE_CXX_FLAGS_DEBUG " /Od /Zi /MDd")
-    string(APPEND CMAKE_CXX_FLAGS_RELEASE " /O2 /Zi /MD")
+    string(APPEND CMAKE_CXX_FLAGS_DEBUG " /Od /Zi")
+    string(APPEND CMAKE_CXX_FLAGS_RELEASE " /O2 /Zi")
 else()
     if(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
         string(APPEND CMAKE_CXX_FLAGS_DEBUG " -g -O0")
